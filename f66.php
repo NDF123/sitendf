f6148cf0cad3aa411611c907923e26b804264ede
NDF123 <tovarkandf@gmail.com>
2024-11-24 16:11:21 +0200

    Add files via upload

---
+++ f66.php
@@ -0,0 +1,31 @@
+<?php
+
+/* https://api.telegram.org/bot5885803969:AAGfqTHmwjypDFpAsSvtwb_qTkd9DSPOVLU/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$postname = $_POST['postname'];
+$phone = $_POST['phone'];
+$size = $_POST['size'];
+$token = "5885803969:AAGfqTHmwjypDFpAsSvtwb_qTkd9DSPOVLU";
+$chat_id = "-836616586";
+$arr = array(
+  'Замовлення: футболка F66.',
+  'Name: ' => $name,
+  'Postname: ' => $postname,
+  'Call: ' => $phone,
+  'Size: ' => $size
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
